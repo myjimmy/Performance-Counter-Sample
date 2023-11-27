b80459dcabff277b0bea76e31178ffcf5d34ae04
Jimmy <myjimmy@protonmail.com>
2023-11-27 11:01:34 -0800

    BASE: Added code to get the category list.

--- PerformanceCounterSample/Program.cs
+++ PerformanceCounterSample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PerformanceCounterSample
 {
@@ -10,7 +11,35 @@ namespace PerformanceCounterSample
     {
         static void Main(string[] args)
         {
+            PerformanceCounterCategory[] categories;
 
+            /* Generate a list of categories registered on this computer. */
+            try
+            {
+                categories = PerformanceCounterCategory.GetCategories();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to get categories on this computer:");
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine("These categories are registered on this computer:");
+
+            // Create and sort an array of category names.
+            string[] categoryNames = new string[categories.Length];
+            int objX;
+            for (objX = 0; objX < categories.Length; objX++)
+            {
+                categoryNames[objX] = categories[objX].CategoryName;
+            }
+            Array.Sort(categoryNames);
+
+            for (objX = 0; objX < categories.Length; objX++)
+            {
+                Console.WriteLine("{0,4} - {1}", objX + 1, categoryNames[objX]);
+            }
         }
     }
 }
